467d3367564a5a8d1b68524b8071b3055351e5cc
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-02-17 14:54:49 +0100

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,2 @@
+raspistill -rot 270 -ev -10 --metering spot -o /home/pi/Desktop/orchid/`date +%Y-%m-%d.%H:%M:%S`.jpg
+raspistill -rot 270 -ev -8 --metering spot -o /home/pi/Desktop/orchid/`date +%Y-%m-%d.%H:%M:%S`.jpg
